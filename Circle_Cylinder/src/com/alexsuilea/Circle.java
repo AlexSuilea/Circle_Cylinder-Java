289cdfba1391fb806415657f5d02cceb2f196b42
Alexandru Suilea <42109259+AlexSuilea@users.noreply.github.com>
2020-07-14 22:44:42 +0300

    Add files via upload

---
+++ Circle_Cylinder/src/com/alexsuilea/Circle.java
@@ -0,0 +1,19 @@
+package com.alexsuilea;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        if(radius<0)
+            radius = 0;
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea(){
+        return Math.PI * getRadius() * getRadius();
+    }
+}
